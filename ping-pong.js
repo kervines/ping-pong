59c3f4c2af7aeece7c22c619e759d8244ea01631
kervines <kervinzero@gmail.com>
2023-04-18 22:33:17 -0300

    ping-pong completo

--- ping-pong.js
+++ ping-pong.js
@@ -2,7 +2,6 @@ const canvasEl = document.querySelector('canvas'),
   canvasCtx = canvasEl.getContext('2d'),
   gapX = 10;
 
-const lineWidth = 15;
 const mouse = { x: 0, y: 0 };
 
 const field = {
@@ -16,7 +15,7 @@ const field = {
 };
 
 const line = {
-  width: lineWidth,
+  width: 15,
   height: field.height,
   draw: function () {
     //desenha linha central do campo
@@ -49,11 +48,19 @@ const leftPaddle = {
 
 const rightPaddle = {
   x: field.width - line.width - gapX,
-  y: 250,
+  y: 0,
   width: line.width,
   height: 200,
+  speed: 5,
   _move: function () {
-    this.y = ball.y;
+    if (this.y + this.height / 2 < ball.y + ball.r) {
+      this.y += this.speed;
+    } else {
+      this.y -= this.speed;
+    }
+  },
+  speedUp: function () {
+    this.speed += 2;
   },
   draw: function () {
     //desenha raquete direita
@@ -65,8 +72,14 @@ const rightPaddle = {
 };
 
 const score = {
-  human: 1,
-  computer: 2,
+  human: 0,
+  computer: 0,
+  increaseHuman: function () {
+    this.human++;
+  },
+  increaseComputer: function () {
+    this.computer++;
+  },
   draw: function () {
     //desenha placar
     canvasCtx.font = 'bold 72px sans-serif';
@@ -79,21 +92,80 @@ const score = {
 };
 
 const ball = {
-  x: 330,
-  y: 100,
+  x: 0,
+  y: 0,
   r: 20,
-  speed: 6,
+  speed: 5,
+  directionX: 1,
+  directionY: 1,
+  _calcPosition: function () {
+    // verifica se o jogador 1 fez um ponto (x > largura do campo)
+    if (this.x > field.width - this.r - rightPaddle.width - gapX) {
+      //verifica se a raquete direita esta na posição y da bola
+      if (
+        this.y + this.r > rightPaddle.y &&
+        this.y - this.r < rightPaddle.y + rightPaddle.height
+      ) {
+        //rebate a bola invertendo o sinal de x
+        this._reverseX();
+      } else {
+        // pontuar jogador 1
+        score.increaseHuman();
+        this._pointUp();
+      }
+    }
+
+    //verificar se o jogador 2 fez um ponto (X < 0)
+    if (this.x < this.r + leftPaddle.width + gapX) {
+      //verifica se a raquete esquerda esta na posição y da bola
+      if (
+        this.y + this.r > leftPaddle.y &&
+        this.y - this.r < leftPaddle.y + leftPaddle.height
+      ) {
+        //rebate a boa invertendo o sinal de x
+        this._reverseX();
+      } else {
+        score.increaseComputer();
+        this._pointUp();
+      }
+    }
+
+    // verifica as laterais superiores e inferiores do campo
+    if (
+      (this.y - this.r < 0 && this.directionY < 0) ||
+      (this.y > field.height - this.r && this.directionY > 0)
+    ) {
+      // rebate a bola invertendo o sinal do eixo Y
+      this._reverseY();
+    }
+  },
+  _reverseX: function () {
+    this.directionX *= -1;
+  },
+  _reverseY: function () {
+    this.directionY *= -1;
+  },
+  _speedUp: function () {
+    this.speed += 3;
+  },
+  _pointUp: function () {
+    this._speedUp();
+
+    this.x = field.width / 2;
+    this.y = field.height / 2;
+  },
   _move: function () {
-    this.x += 1 * this.speed;
-    this.y += 1 * this.speed;
+    this.x += this.directionX * this.speed;
+    this.y += this.directionY * this.speed;
   },
   draw: function () {
     //desenha bolinha
     canvasCtx.fillStyle = '#ffffff';
     canvasCtx.beginPath();
-    canvasCtx.arc(this.x, this.y, this.r, 2 * Math.PI, false);
+    canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
     canvasCtx.fill();
 
+    this._calcPosition();
     this._move();
   },
 };
